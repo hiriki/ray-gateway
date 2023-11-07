3fc9b46fb21adaba416c08ec8052ab58cd68eb56
Ray <2020215059@stu.cqupt.edu.cn>
2023-11-07 21:51:45 +0800

    feat：自定义请求对象 GatewayRequest 封装

---
+++ common/src/main/java/cn/ray/gateway/common/utils/TimeUtil.java
@@ -0,0 +1,37 @@
+package cn.ray.gateway.common.utils;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author Ray
+ * @date 2023/11/7 21:10
+ * @description
+ */
+public class TimeUtil {
+
+    private static volatile long currentTimeMillis;
+
+    static {
+        currentTimeMillis = System.currentTimeMillis();
+        Thread daemon = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    currentTimeMillis = System.currentTimeMillis();
+                    try {
+                        TimeUnit.MILLISECONDS.sleep(1);
+                    } catch (Throwable e) {
+
+                    }
+                }
+            }
+        });
+        daemon.setDaemon(true);
+        daemon.setName("common-fd-time-tick-thread");
+        daemon.start();
+    }
+
+    public static long currentTimeMillis() {
+        return currentTimeMillis;
+    }
+}
